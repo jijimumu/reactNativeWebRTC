e8b2a18316551bf4653ed30ec2a6662f96e4be9a
jim.zhang <755163jim@gmail.com>
2016-02-24 14:17:00 +0800

    implement node socket server

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var http = require('http');
 var fs = require('fs');
 var socket_io = require('socket.io');
 var port = '8000';
-var ip = '192.168.1.63';
+var ip = 'localhost';
 function onRequest(request,response){
     console.log('Request recieved');
     response.writeHead(200,{'Content-Type':'text/html'});
-    fs.readFile('index.html','utf8',function(){
+    fs.readFile('index.html','utf8',function(err,data){
         if(err){
             return console.log(err);
         }
@@ -17,3 +17,21 @@ function onRequest(request,response){
         response.end(data);
     });
 }
+
+var server = http.createServer(onRequest).listen(port,ip);
+var io = socket_io.listen(server);
+
+io.sockets.on('connection',function(socket){
+    socket.on('offer',function(data){
+        socket.broadcast.emit('offer',{sdp: data.sdp});
+    })
+    socket.on('answer',function(data){
+        socket.broadcast.emit('answer',{sdp: data.sdp});
+    })
+    socket.on('ice',function(data){
+        socket.broadcast.emit('ice',{candidate: data.candidate});
+    })
+    socket.on('hangup',function(){
+        socket.broadcast.emit('hangup',{});
+    })
+});
